00bf930a4b7eb26f478e6c2f3849e2274793a65b
Rajalakshmi-PK <30773062+Rajalakshmi-PK@users.noreply.github.com>
2017-08-19 21:44:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article1'));
+  res.send('Article1');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
